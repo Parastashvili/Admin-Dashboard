8663ce5207b96be522b30842d2c803eb1d7e3413
Levan Parastashvili <levani.parastashvili@gmail.com>
2023-08-10 23:35:56 +0400

    a

--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,15 @@ import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
   authDomain: "rentio-32618.firebaseapp.com",
+  databaseURL:
+    "https://rentio-32618-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "rentio-32618",
   storageBucket: "rentio-32618.appspot.com",
   messagingSenderId: "434474456812",
   appId: "1:434474456812:web:f52cad82318b93ddafb7c9",
   measurementId: "G-RV12EZ3FF2",
 };
+
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const db = getFirestore(app);
