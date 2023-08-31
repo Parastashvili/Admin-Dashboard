bfd403b0ed2c1d540debae27dd0087534e42c619
Levan Parastashvili <levani.parastashvili@gmail.com>
2023-08-31 13:12:04 +0400

    updated info

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -33,11 +33,8 @@ const New = ({ inputs, title }) => {
     const uploadFile = () => {
       const name = new Date().getTime() + file.name;
       console.log(name);
-      
       const storageRef = ref(storage, file.name);
-
       const uploadTask = uploadBytesResumable(storageRef, file);
-
       uploadTask.on(
         "state_changed",
         (snapshot) => {
